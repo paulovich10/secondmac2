85192ea597139fb710a92ceebf35fba45e9d053d
Paula <>
2019-11-06 18:21:53 +0100

    solucion merge conflicts

--- macangular/src/app/productos.service.ts
+++ macangular/src/app/productos.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { PRODUCTOS } from './db/productos.bd';
 import { MODELOS } from './db/productos.bd';
-<<<<<<< HEAD
+
 import { CAPACIDADES } from './db/productos.bd';
 import { Producto } from './models/producto.model';
 import { Modelo } from './models/modelo.model';
 import { Capacidad } from './models/capacidad.model';
-=======
-import { Producto } from './models/producto.model';
-import { Modelo } from './models/modelo.model';
->>>>>>> 2a5b619650457364eac64a78270b5be848f1e707
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -22,11 +20,7 @@ export class ProductosService {
   getAllProductos(): Producto[] {
     return (PRODUCTOS);
   }
-getAllModelos(): Promise<Modelo[]>{
-  return new Promise <Modelo[]> ((resolve, reject)=>{
-    resolve (MODELOS);
-  })
-}
+
 
   getAllModelos(): Modelo[] {
     return (MODELOS)
--- macangular/src/app/registration/registration.component.ts
+++ macangular/src/app/registration/registration.component.ts
@@ -25,7 +25,7 @@ export class RegistrationComponent implements OnInit {
   }
 
   ngOnInit() {
-<<<<<<< HEAD
+
 
     this.iniciarArray();
     //al seleccionar en el desplagabla un producto, me traigo el id y se lo paso por parametro a 
@@ -42,35 +42,13 @@ export class RegistrationComponent implements OnInit {
 
   onSelect(productoId) {
     this.arrModelos = this.productoServicio.getAllModelos().filter((modelo) => modelo.productoId == productoId)
-=======
-    this.productoServicio.getAllProductos()
-      .then(arr => {
-        this.arrProductos = arr
-      })
-  }
 
-  iniciarArray() {
-    this.productoServicio.getAllProductos()
-      .then(arr => {
-        this.arrProductos = arr
-
-      })
 
   }
 
-  async onSelect(productoId) {
-    this.arrModelos = await this.productoServicio.getAllModelos()
-    let filtrarById = (modelo) =>{
-      if (modelo.productoId == productoId) return true
-      return false
-    }
-    try{
-      this.arrModelos = this.arrModelos.filter(filtrarById)
-      console.log(this.arrModelos)
-    }catch(err){
-      console.log(err)
-    }
->>>>>>> 2a5b619650457364eac64a78270b5be848f1e707
-  }
+
 }
+
+
+
 
